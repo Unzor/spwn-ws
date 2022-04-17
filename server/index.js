2bbc4b700829969b0402fbc474bded85b567bb9e
Unzor <71789103+Unzor@users.noreply.github.com>
2022-04-17 09:58:51 -0500

    improve code i guess

--- server/index.js
+++ server/index.js
@@ -14,39 +14,45 @@ app.get("/", function(req, res) {
 
 app.post("/ws", function(req, res) {
     var server = new WebSocket(req.body.url);
-    console.log(req.body)
+    server.on("error", (e) => {
+        res.send(e);
+    })
+    server.end = function() {
+        res.end();
+    }
     server.onmessage = function(e) {
         res.write(e.data + "\n");
-        server.end = function() {
-            res.end();
-        }
     }
     server.onopen = function() {
         eval(req.body.messages).forEach(function(e, i) {
             if (req.body.timeout) {
                 setTimeout(function() {
-                    server.send(e);
-                    console.log(i);
+                    if (typeof e == "string") {
+                        server.send(e);
+                    }
                     if (i == eval(req.body.messages).length - 1) {
                         setTimeout(function() {
                             server.end();
                             server.terminate();
-                        }, parseInt(req.body.endAfter) || parseInt(req.body.timeout))
+                        }, parseInt(req.body.endAfter) || parseInt(req.body.timeout) || 100)
                     }
                 }, parseInt(req.body.timeout) * i);
             } else {
-                server.send(e);
-                console.log(i);
+                if (typeof e == "string") {
+                    server.send(e);
+                }
                 if (i == eval(req.body.messages).length - 1) {
-                    server.send(eval(req.body.messages)[eval(req.body.messages).length]);
+
                     if (req.body.endAfter) {
                         setTimeout(function() {
                             server.end()
                             server.terminate();
                         }, parseInt(req.body.endAfter))
                     } else {
-                        server.end()
-                        server.terminate();
+                        setTimeout(function() {
+                            server.end()
+                            server.terminate();
+                        }, 100)
                     }
                 }
             }
